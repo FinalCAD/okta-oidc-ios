abf1ca0c3936b708af638293ced32d774ddf89f7
Antoine Gamond <antoine.gamond@finalcad.com>
2020-11-09 21:29:38 +0100

    Test with zip on GitHub

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
         .library(name: "OktaOidc", targets: ["OktaOidc"])
     ],
     targets: [
-        .binaryTarget(name: "OktaOidc", path: "artifact/OktaOidc.xcframework")
+        .binaryTarget(name: "OktaOidc", url: "https://github.com/FinalCAD/okta-oidc-ios/releases/download/untagged-a9aa087384d69b46fb1c/OktaOidc-3.9.2.xcframework.zip", checksum: "22d2a048a23a5d804ca55b9545e9ecb72d1c9caf002b1125736abd6bf38eafac")
     ]
 )
